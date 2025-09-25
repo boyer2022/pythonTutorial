b56698f7c070d622036b328335dcf384c6238f3a
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-25 17:18:19 -0500

    Strings added with comments

---
+++ Strings/strings.py
@@ -0,0 +1,22 @@
+course = "Python Programming"
+# Triple quotes
+message = """
+Hi John,
+This is the Complaint Department calling about your 
+recent concern.
+Blah Blah Blah
+"""
+
+# Function 
+# A reuseable piece of code to perform various tasks
+    # () means "Calling an arguement"
+    # Takes a variable as an arguement in the ()
+print(len(course))
+    # 18 letters in length for course
+
+# Access a specific character- []
+# Starts at 0 for first character, also -1 for 1st character of end of program
+# Index range of characters - [0:3] This will return 3 characters starting with position 0
+# Index of [0:] Returns full string, [:3] = first 3 characters, [:] is full string
+print(course[0])   
+
