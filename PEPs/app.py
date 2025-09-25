f258bd4d2c89e7a96542bbd2a03cd93c83585e8a
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-25 15:00:27 -0500

    Added examples for formating, comments for AutoSave

--- PEPs/app.py
+++ PEPs/app.py
@@ -1,8 +1,12 @@
 # Example given for pep8 formatting
 
 x=1
+y=2
+unit_price=3
 # PEP8 should be: x = 1
 # Go to Command Palette,
 # Search for Format document.
 # Message to install autopep8, or use extensions panel and install/search
 # Once installed, reopen Command Palette, Format Document
+# To auto format changes- Code, Preferences, Settings
+# In Search(formatOnSave, check box)
