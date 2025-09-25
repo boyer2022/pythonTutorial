3cbf71c3a8b7110adee70ec3be8134a874fa2295
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-25 12:40:40 -0500

    Installed autoPEP8 with notes

---
+++ PEPs/app.py
@@ -0,0 +1,8 @@
+# Example given for pep8 formatting
+
+x=1
+# PEP8 should be: x = 1
+# Go to Command Palette,
+# Search for Format document.
+# Message to install autopep8, or use extensions panel and install/search
+# Once installed, reopen Command Palette, Format Document
