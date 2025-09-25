cf23858c8b9f188d1ce38a4ced0efafc0303fa3f
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-25 16:29:15 -0500

    Added variables with examples

---
+++ Variables/variables.py
@@ -0,0 +1,9 @@
+# Whole number integer
+students_count = 1000
+# Foat
+rating = 4.99
+# Boolean
+# Boolean is True or False, case sensitive
+is_published = False
+# String
+course_name = "Python Programming"
