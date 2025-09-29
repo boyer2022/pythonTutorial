c0b7a67de5574c5816db1a83654c6ec78fec7015
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-28 19:14:02 -0500

    Added numbers and arithmetic

---
+++ Numbers/numberTypes.py
@@ -0,0 +1,32 @@
+# Three types of numbers
+    # Integers
+x = 1
+    # Floats
+x = 1.1
+    # Complex
+x = 1 + 2j
+
+# Standard arithmetic operations
+    # Addition
+print(10 + 3)
+    #  Subtraction
+print(10 - 3)
+    # Multiplication
+print(10 * 3)
+    # Division (2 types)
+print(10 / 3)   # Floating point
+print(10 // 3)  # Whole number
+    #  Modulus
+print(10 ** 3)  # Exponent, less left to the poser of right
+
+# Augmentet assignment operator
+x = 10
+    # Increment by 3
+# x = x + 3
+    # Or (Uncomment each)
+# x += 3
+# x -= 3
+# x *= 3
+# x /= 3
+print(x)
+
