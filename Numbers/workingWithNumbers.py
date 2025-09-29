13f2cb8bad9f247b7704c7f82b577098ea6d0b93
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-28 20:13:36 -0500

    Added Math functions with examples

---
+++ Numbers/workingWithNumbers.py
@@ -0,0 +1,11 @@
+# Math Module
+import math
+# Using math. function gives options for math module
+print(math.ceil(2.2))
+
+# Working with Rounding numbers
+round(2.9)
+print(round(2.9))
+# Absolute value
+print(abs(-2.9))
+
