31036b94a12bbdaf789fe942f280bad4dcdaa1d5
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-29 16:03:11 -0500

    Added conditionals if statements

---
+++ Fundamentals/conditionals.py
@@ -0,0 +1,14 @@
+#  Conditional Statements
+temperature = 15    # Change value to see program run differently
+if temperature > 30: # Always use a colon during if statements to indent
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20: # Else if statement
+    print("It's nice")
+# If neither is true
+else:
+    print("Brrr.")
+    print("Maybe you should move!")
+print("Done")
+
+
