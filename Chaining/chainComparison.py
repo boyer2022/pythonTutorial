4e0e39637eee70cc8e41e556bd6d099048c36a62
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-29 16:58:24 -0500

    Chaining examples

---
+++ Chaining/chainComparison.py
@@ -0,0 +1,16 @@
+# Chaining Comparison Operators
+
+#  Implemetation of a rule: age between 16 and 65
+age = 22
+    # Uncomment to test
+# if age >= 18 and age < 65:
+#     print("Eligible")
+
+# Writing in Math
+# 18 <= age < 65
+
+
+# Writing cleaner code in python
+    # Comment to test line 6
+if 18 <= age < 65:
+    print("Eligible")
