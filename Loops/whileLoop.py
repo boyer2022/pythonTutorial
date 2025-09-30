0a4bcd9a0842ab4b4a58faaa3e119c0074f2e892
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-29 21:08:52 -0500

    While Loop and while loop example

---
+++ Loops/whileLoop.py
@@ -0,0 +1,8 @@
+# While Loop
+# Used for repeating as long as the condition is true
+number = 100
+while number > 0:
+    print(number)
+    # number = number // 2
+    # or
+    number //= 2
---
+++ Loops/whileLoopExample.py
@@ -0,0 +1,6 @@
+#  To run: 
+    # Terminal, python3, whileLoopExample.py
+command = ""
+while command.lower() != "quit":
+    command = input(">")
+    print("ECHO", command)
