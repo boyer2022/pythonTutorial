38520e272d4e2190c7c4a5e67a64aee869dd06de
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-29 20:38:52 -0500

    Iterables with complex type object

--- Loops/iterables.py
+++ Loops/iterables.py
@@ -3,3 +3,8 @@ for x in range(5):
 # Understanding the type object
 print(type(5))
 print(type(range(5))) # Object type of range, complex type
+
+#Iterable (means to iterate over it)
+for y in "Python":
+    print(y)
+
