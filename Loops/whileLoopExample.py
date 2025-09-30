03e20b1a4ba4dea08b776b62ed8d36aafc6f0ea7
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-30 12:33:49 -0500

    Added comment

--- Loops/whileLoopExample.py
+++ Loops/whileLoopExample.py
@@ -1,6 +1,6 @@
 #  To run: 
     # Terminal, python3, whileLoopExample.py
 command = ""
-while command.lower() != "quit":
+while command.lower() != "quit": # Always creates the user's input to lowercase
     command = input(">")
     print("ECHO", command)
