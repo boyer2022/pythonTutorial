8f15453f6627ab133f323c69c5414f32fa9ceaa9
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-30 13:17:41 -0500

    Functions

---
+++ Functions/functionExamples.py
@@ -0,0 +1,18 @@
+# Built in python functions
+    # print()
+    # round()
+
+# Building own funtions
+    # Purpose: Break code into smaller, reusable chunks
+
+# def = define
+            # parameters
+def greet(first_name, last_name):
+    print(f"Hi there {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+# Calling the function
+    # arguements
+greet("Matt", "Boyer")
+
---
+++ Functions/optionalParameter.py
@@ -0,0 +1,18 @@
+# Simplified greet function
+def greet(name):
+    print(f"Hi {name}")
+
+
+greet("Matt")
+
+
+# Functions do :
+# 1 - Perform a task
+# 2 - Return a value
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+message = get_greeting("Matt")
+
