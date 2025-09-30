849cf01ac5b2ef0c8bf2b7a94f8f03c845167e07
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-30 17:02:14 -0500

    Examples of arguements

---
+++ Functions/argumentsexample.py
@@ -0,0 +1,11 @@
+def increment(number, by):
+    return number + by
+
+results = increment(2, 1)
+print (results)
+
+# Simplify
+print(increment(2, 1)) 
+
+# More readable, by using keyword arguements
+print(increment(2, by=1))
